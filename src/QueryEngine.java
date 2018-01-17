4fd1d73061da85891bb633d9f9f2a4bf3431c508
gaurav <gaurav.mahajan@students.iiit.ac.in>
2018-01-17 14:49:33 +0530

    adding first version of identifying select statements and table sources

--- src/QueryEngine.java
+++ src/QueryEngine.java
@@ -1,9 +1,31 @@
 
+
+import gudusoft.gsqlparser.*;
+import gudusoft.gsqlparser.stmt.TSelectSqlStatement;
+
 public class QueryEngine {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("query engine will go here");
+		TGSqlParser sqlParser = new TGSqlParser(EDbVendor.dbvmysql);
+		sqlParser.sqltext = "select p,q,r from A,B";
+		if(sqlParser.parse() == 0)
+			System.out.println("parsed");
+		
+		TSelectSqlStatement statement = (TSelectSqlStatement) sqlParser.sqlstatements.get(0);
+		
+		int tableCount = statement.joins.size();
+		System.out.println("table count" + tableCount);
+		
+		
+		
+		int columnCount = statement.getResultColumnList().size(), pos = -1;
+		
+		System.out.println("column count " + columnCount);
+		
+		
+		
+		
 
 	}
 
